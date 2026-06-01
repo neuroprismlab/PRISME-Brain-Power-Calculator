2f5029f316d8e36b46fe183fc889545861549fe8
Fabricio Cravo <fabricio.cravo1@outlook.com>
2026-06-01 11:29:05 -0400

    Updated setparams for cluster hcp corr calculation

--- setparams.m
+++ setparams.m
@@ -56,15 +56,15 @@ Params.nbs_dir = './NBS1.2';
 Params.other_scripts_dir='./NBS_benchmarking/support_scripts/';
 
 %%% Resampling parameters %%%
-Params.parallel = false; % run stuff sequentially or in parallel
-Params.n_workers = 5; % num parallel workers for parfor, best if # workers = # cores
+Params.parallel = true; % run stuff sequentially or in parallel
+Params.n_workers = 10; % num parallel workers for parfor, best if # workers = # cores
 Params.n_repetitions = 100;  % 500 recommended
 Params.batch_size = 10;
  
 
 %% Skip some tests - change ranges or the function
 % Tests to skip accepts functions
-ranges = {[0, 2]};
+ranges = {[0, 0]};
 Params.tests_to_skip = @(x) any(cellfun(@(r) (x >= r(1)) && (x <= r(2)), ranges));
 
 
@@ -94,7 +94,7 @@ Params.cluster_size_type = 'Extent'; % 'Intensity' | 'Extent'
 %%%%% DEVELOPERS ONLY %%%%%
 % Use a small subset of permutations for faster development -- inappropriate for inference
 
-Params.testing = true;
+Params.testing = false;
 Params.test_n_perms = 10;
 Params.test_n_repetitions = 5;
 Params.test_n_workers = 1;
